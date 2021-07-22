40d474686a6d4192445e5cdaa06775f6e770b68c
Tyler Weiglein <tweiglein@Red-Cedar.local>
2021-07-22 09:41:30 -0400

    Finished code to generate TRI data plot

--- tri_data/tri_data_plot.R
+++ tri_data/tri_data_plot.R
@@ -17,9 +17,38 @@ rm(list = ls(all.names = TRUE))
 # Load package(s)
 
 library(lubridate)
+library(scales)
 library(tidyverse)
 
 # Read in data
 
-tri_data <- read_csv("data_raw/RAAP_TRI_data.csv")
+tri_data <- read_csv("tri_data/data_raw/RAAP_TRI_data.csv")
 
+
+# Create TRI Data Plot ----------------------------------------------------
+
+tri_data <- tri_data %>% 
+  select(c(REPORTING_YEAR, AIR_TOTAL_RELEASE, WATER_TOTAL_RELEASE, LAND_TOTAL_RELEASE, TOTAL_OFF_SITE_RELEASE)) %>%
+  rename(year = REPORTING_YEAR,
+         air = AIR_TOTAL_RELEASE,
+         water = WATER_TOTAL_RELEASE,
+         land = LAND_TOTAL_RELEASE,
+         off_site = TOTAL_OFF_SITE_RELEASE) %>% 
+  pivot_longer(cols = c(air, water, land, off_site),
+               names_to = "media",
+               values_to = "release_lbs") %>% 
+  mutate(media = ordered(as.factor(media), levels = c("air", "water", "land", "off_site"))) %>% 
+  group_by(year, media) %>% 
+  summarize(tot_release_lbs = sum(release_lbs))
+
+ggplot(tri_data, aes(x = year, y = tot_release_lbs, fill = media)) +
+  geom_bar(position = position_stack(reverse = TRUE), stat = "identity") +
+  scale_fill_manual(labels = c("Air", "Water", "Land", "Off-Site"),
+                    values = c("#CCE3FC", "#1A6692", "#395420", "#DA6D1D")) +
+  scale_y_continuous(labels = comma) +
+  labs(title = "RAAP TRI Releases by Year",
+       x = "Year",
+       y = "Releases (lb)",
+       fill = "Key:") +
+  theme_bw()
+  
