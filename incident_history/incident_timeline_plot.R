c3ecd549fda87bb82bac244e5c7295cbae5b4f4f
Tyler Weiglein <tweiglein@Red-Cedar.local>
2021-08-16 10:33:03 -0400

    Overhauled code to improve plot appearance

--- incident_history/incident_timeline_plot.R
+++ incident_history/incident_timeline_plot.R
@@ -2,14 +2,12 @@
 
 # File Name: incident_timeline_plot.R
 # Created: 08 Jul 2021 by Tyler Weiglein
-# Last Modified: 22 Jul 2021 by Tyler Weiglein
+# Last Modified: 16 Aug 2021 by Tyler Weiglein
 
 # Purpose: To create a timeline plot for incidents at RAAP.
 
 # Code based off instructions given here: 
 # https://benalexkeen.com/creating-a-timeline-graphic-using-r-and-ggplot2/
-
-# **NOTE**: Currently using subset of incident data to test functionality.
 
 
 # Preliminaries -----------------------------------------------------------
@@ -21,73 +19,125 @@ rm(list = ls(all.names = TRUE))
 
 # Load package(s)
 
+library(ggtext)
 library(lubridate)
 library(tidyverse)
 
 # Read in data
 
-incident_data <- read_csv("incident_history/data_raw/incident_data_test.csv")
+incident_data <- read_csv("incident_history/data_raw/RAAP_incident_data_2018_Aug2021.csv")
 
 
 # Create Timeline Plot ----------------------------------------------------
 
 # Modify data types in data frame
 
-incident_data <- incident_data %>% 
-  mutate(incident_type = as.factor(incident_type),
+incident_data <- incident_data %>%
+  select(release_type, date_occurred, description) %>% 
+  mutate(release_type = as.factor(release_type),
          date_occurred = mdy(date_occurred),
-         date_public_notified = mdy(date_public_notified),
-         month_year_occurred = paste(month(date_occurred), year(date_occurred)))  
+         month_year_occurred = paste(month(date_occurred), year(date_occurred))) %>% 
+  filter(release_type != "Sediment")
 
-# **NOTE**: Will also need to change amount to numeric
+# Add line breaks to description names to make sure descriptions do not overlap on timeline
+
+incident_data$description <- str_replace(incident_data$description,
+                                         "Anhydrous ammonia",
+                                         "Anhydrous\nammonia")
+
+incident_data$description <- str_replace(incident_data$description,
+                                         "Propellant slurry",
+                                         "Propellant\nslurry")
+
+incident_data$description <- str_replace(incident_data$description,
+                                         "Oil/Hydraulic oil",
+                                         "Oil/\nHydraulic oil")
+
+incident_data$description <- str_replace(incident_data$description,
+                                         "Crowder tank flooding",
+                                         "Crowder tank\nflooding")
+
+incident_data$description <- str_replace(incident_data$description,
+                                         "Caustic rinse water \\(pH ~ 11\\)",
+                                         "Caustic\nrinse water")
+
+incident_data$description <- str_replace(incident_data$description,
+                                         "Reclamation water",
+                                         "Reclamation\nwater")
+
+incident_data$description <- str_replace(incident_data$description,
+                                         "Industrial wastewater \\(\"minute\" quantities of nitrocellulose\\)",
+                                         "Industrial\nwastewater")
+
+incident_data$description <- str_replace(incident_data$description,
+                                         "Industrial wastewater \\(77 ppm nitroglycerin\\)",
+                                         "Industrial\nwastewater")
+
+incident_data$description <- str_replace(incident_data$description,
+                                         "Brine water",
+                                         "Brine\nwater")
+
+incident_data$description <- str_replace(incident_data$description,
+                                         "Industrial wastewater$",
+                                         "Industrial\nwastewater")
+
+incident_data$description <- str_replace(incident_data$description,
+                                         "Nitric acid$",
+                                         "Nitric\nacid")
+
+incident_data$description <- str_replace(incident_data$description,
+                                         "Industrial residuals",
+                                         "Industrial\nresiduals")
+
+incident_data$description <- str_replace(incident_data$description,
+                                         "Nitric acid/sulfuric acid mixture",
+                                         "Nitric acid/\nsulfuric acid\nmixture")
+
+incident_data$description <- str_replace(incident_data$description,
+                                         "Industrial process wastewater and sewage",
+                                         "Industrial\nprocess wastewater\nand sewage")
+
+incident_data$description <- str_replace(incident_data$description,
+                                         "Diesel fuel",
+                                         "Diesel\nfuel")
+
+incident_data$description <- str_replace(incident_data$description,
+                                         "Diethyl ether",
+                                         "Diethyl\nether")
+
+incident_data$description <- str_replace(incident_data$description,
+                                         "Acidic wastewater",
+                                         "Acidic\nwastewater")
 
 # Create positions and directions data frame
 
-positions <- c(0.5, -0.5, 1.0, -1.0, 1.5, -1.5)
+positions <- c(0.85, -0.85, 1.7, -1.7, 2.55, -2.55)
 directions <- c(1, -1)
 
 line_pos <- data.frame(
-  "date_occurred" = unique(incident_data$date_occurred),
-  "position" = rep(positions, length.out = length(unique(incident_data$date_occurred))),
-  "direction" = rep(directions, length.out = length(unique(incident_data$date_occurred)))
+  "position" = rep(positions, length.out = length(incident_data$date_occurred)),
+  "direction" = rep(directions, length.out = length(incident_data$date_occurred))
 )
 
 # Join "incident_data" and "line_pos" data frames
 
-incident_data <- left_join(incident_data, line_pos, by = "date_occurred")
-
-# Offset text as necessary for incidents occurring in same month
-
-text_offset <- 0.1
-
-incident_data$month_count <- ave(incident_data$month_year_occurred == incident_data$month_year_occurred,
-                                 incident_data$month_year_occurred,
-                                 FUN = cumsum)
-incident_data$text_position <- (incident_data$month_count * text_offset * incident_data$direction) + incident_data$position
+incident_data <- bind_cols(incident_data, line_pos)
 
 # Create data frame for all months in plotting timeframe
 
 month_buffer <- 2
 
-month_date_range <- seq(min(incident_data$date_occurred) - months(month_buffer),
-                        max(incident_data$date_occurred) + months(month_buffer),
+month_date_range <- seq(ym(format(min(incident_data$date_occurred), "%Y-%m")) - months(month_buffer),
+                        ym(format(max(incident_data$date_occurred), "%Y-%m")) + months(month_buffer - 1),
                         by = "month")
 month_format <- format(month_date_range, "%b")
 month_df <- data.frame(month_date_range, month_format)
 
-# Create data frame for all months in plotting timeframe
-
-# year_date_range <- seq(min(incident_data$date_occurred) - months(month_buffer),
-#                        max(incident_data$date_occurred) + months(month_buffer),
-#                        by = "year")
-# 
-# ^Above code commented out b/c year 2021 excluded
+# Create data frame for all years in plotting timeframe
 
 year_date_range <- as.Date(
-  intersect(
-    ceiling_date(month_date_range, unit = "year"),
-    floor_date(month_date_range, unit = "year")
-  ), origin = "1970-01-01"
+  unique(floor_date(month_date_range, unit = "year")),
+  origin = "1970-01-01"
 )
 year_format <- format(year_date_range, "%Y")
 year_df <- data.frame(year_date_range, year_format)
@@ -95,9 +145,10 @@ year_df <- data.frame(year_date_range, year_format)
 # Create timeline plot
 
 timeline_plot <- ggplot(incident_data,
-                        aes(x = date_occurred, y = 0, col = incident_type, label = chemical)) +
-  labs(col = "Incident Type") +
-  scale_color_discrete(labels = c("Fire", "Overflow", "Spill/Leak")) +
+                        aes(x = date_occurred, y = 0, label = description)) +
+  labs(fill = "Incident Type:") +
+  scale_fill_manual(values = c("#619CFF", "#F8766D", "#00BA38"),
+                    labels = c("Chemical Spill", "Fire", "Other")) +
   theme_classic() +
   
   # Plot horizontal black line for timeline
@@ -106,16 +157,20 @@ timeline_plot <- ggplot(incident_data,
              color = "black",
              size = 0.3) +
   
-  # Plot vertical segment lines for milestones
+  # Specify y-axis limits
   
-  geom_segment(data = incident_data[incident_data$month_count == 1, ],
-               aes(y = position, yend = 0, xend = date_occurred),
+  ylim(1.3 * min(incident_data$position), 1.3 * max(incident_data$position)) +
+  
+  # Plot vertical segment lines for incidents
+  
+  geom_segment(aes(y = position, yend = 0, xend = date_occurred),
                color = "black",
                size = 0.2) +
   
   # Plot scatter points at zero and date
   
-  geom_point(aes(y = 0),
+  geom_point(aes(y = 0, fill = release_type),
+             shape = 21,
              size = 3) +
 
   # Don't show axes, appropriately position legend
@@ -128,32 +183,51 @@ timeline_plot <- ggplot(incident_data,
         axis.title.y = element_blank(),
         axis.ticks.x = element_blank(),
         axis.ticks.y = element_blank(),
-        legend.title = element_text(size = 12),
+        legend.title = element_text(size = 12, face = "bold"),
         legend.text = element_text(size = 12),
-        legend.position = "bottom"
-        ) + 
+        legend.position = "bottom") + 
   
   # Show text for each month
   
-  geom_text(data = month_df,
-            aes(x = month_date_range, y = -0.2, label = month_format),
-            size = 4,
-            vjust = 0.5,
-            color = "black",
-            angle = 90) +
+  geom_richtext(data = month_df,
+                aes(x = month_date_range, y = -0.1, label = month_format),
+                size = 3.25,
+                hjust = 1,
+                vjust = 0.5,
+                color = "black",
+                angle = 90,
+                label.padding = unit(-0.03, "lines"),
+                label.size = NA) +
   
   # Show year text
   
-  geom_text(data = year_df,
-            aes(x = year_date_range, y = -0.5, label = year_format, fontface = "bold"),
+  geom_label(data = year_df,
+            aes(x = year_date_range, y = -0.65, label = year_format, fontface = "bold"),
             size = 4,
-            color = "black") +
+            color = "black",
+            label.padding = unit(0.15, "lines"),
+            label.size = NA) +
   
-  # Show text for each milestone
-  
-  geom_text(aes(y = text_position, label = chemical),
-            col = "black",
-            size = 4)
+  # Show text for each incident
 
+  geom_label(data = filter(incident_data, position > 0),
+             aes(y = position, label = description),
+             col = "black",
+             fill = "gray97",
+             label.padding = unit(0.3, "lines"),
+             size = 3.25,
+             vjust = 0) +
 
+# Show text for each incident (below timeline)
 
+  geom_label(data = filter(incident_data, position < 0),
+             aes(y = position, label = description),
+             col = "black",
+             fill = "gray97",
+             label.padding = unit(0.3, "lines"),
+             size = 3.25,
+             vjust = 1)
+
+# Save figure
+
+ggsave("incident_history/fig/incident_timeline_plot.png", timeline_plot, width = 12, height = 6, units = "in", dpi = 1200)
